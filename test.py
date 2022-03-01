ed555b452793009dce1966748619a46c4a89dfa3
rumesh <rumesh.m.s16@gmail.com>
2022-03-01 16:26:20 +0530

    aded a new test

--- test.py
+++ test.py
@@ -1,9 +1,41 @@
+from io import TextIOWrapper
+
 import pexpect
 
 # pip install pexpect
-client = pexpect.spawn('java -jar ChatClient.jar  --host localhost --port 9991 -i "Alice"')
-client.sendline("mypassword")
-client.expect("defc")
+import sys
+
+num_servers = 3
+num_clients = 3
+servers = []
+clients = []
+
+
+def start_servers(n):
+    for i in range(1, num_servers + 1):
+        server = pexpect.spawn("java -jar target/ChatServer-1.0.0-jar-with-dependencies.jar s" + str(i) + " conf",
+                               encoding='utf-8')
+        server.logfile = sys.stdout
+        servers.append(server)
+
+
+def start_clients(n):
+    for i in range(num_clients):
+        client = pexpect.spawn('java -jar ChatClient.jar  --host localhost --port 4444 -i "Alice" -d', encoding='utf-8')
+        client.logfile = sys.stdout
+        clients.append(client)
+
+
+try:
+    start_servers(num_servers)
+    start_clients(num_clients)
+    clients[0].sendline("Hello")
+    clients[0].expect("Hello")
+finally:
+    for s in servers:
+        s.close(force=True)
+    for c in clients:
+        c.close(force=True)
 
 if __name__ == '__main__':
     pass
