09c5814194c1d3431004c0e60ec527580be99f46
Thamindu Randil <45028707+ThaminduR@users.noreply.github.com>
2022-03-03 13:27:14 +0530

    Json parse request

--- src/main/java/Server/ServerHandler.java
+++ src/main/java/Server/ServerHandler.java
@@ -3,9 +3,14 @@ package Server;
 import ClientHandler.ClientHandler;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class ServerHandler extends Thread {
     //TODO: Implement Server-server communication (Similar to client-server)
@@ -22,10 +27,21 @@ public class ServerHandler extends Thread {
     public void run(){
         try{
             while(true){
-//                Socket serverSocket = ser
+                Socket serverSocket = serverCoordSocket.accept();
+                InputStream inputFromClient = serverSocket.getInputStream();
+                Scanner serverInputScanner = new Scanner(inputFromClient, String.valueOf(StandardCharsets.UTF_8));
+                String line = serverInputScanner.nextLine();
+                logger.debug("Received: " +line);
+                JSONParser jsonParser = new JSONParser();
+                JSONObject jsonPayload = (JSONObject) jsonParser.parse(line);
+                resolveServerRequest(jsonPayload);
             }
         } catch (Exception e) {
 
         }
+    }
+
+    private void resolveServerRequest(JSONObject jsonPayload){
+
     }
 }
