612f22c6db5e6f3a645c34bee0c8f9014e6c2e44
rumesh <rumesh.m.s16@gmail.com>
2022-03-04 19:02:45 +0530

    added a trap for the processes to exit when the script is killed

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
+trap "kill 0" EXIT
 mvn clean install
 java -jar target/ChatServer-1.0.0-jar-with-dependencies.jar s1 conf &
 java -jar target/ChatServer-1.0.0-jar-with-dependencies.jar s2 conf &
 java -jar target/ChatServer-1.0.0-jar-with-dependencies.jar s3 conf &
+wait
